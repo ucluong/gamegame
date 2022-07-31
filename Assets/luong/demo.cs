7a72a946244fb3ae5c5d7ee31e5559dc75e6e01c
ucluong <108748357+ucluong@users.noreply.github.com>
2022-07-31 20:51:33 +0800

    backcload
    game , backcload

---
+++ Assets/luong/demo.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D colTarget)
+    {
+        
+        Debug.Log("da va cham voi nhau ");
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+
+}
